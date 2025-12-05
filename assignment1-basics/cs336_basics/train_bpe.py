d8501385ec7e96ca40191ed08d1cba76a431be38
SMS915 <171641382+SMS915@users.noreply.github.com>
2025-12-05 13:54:12 +0800

    modified bpe training script and code for comparison, update readme

--- assignment1-basics/cs336_basics/train_bpe.py
+++ assignment1-basics/cs336_basics/train_bpe.py
@@ -4,7 +4,9 @@ import os
 import time
 import datetime
 from tqdm import tqdm
+from typing import Literal
 from cs336_basics.bpe_fast import BPETokenizer
+from cs336_basics.bpe_naive import train_bpe_run
 
 
 def main():
@@ -15,6 +17,8 @@ def main():
     parser.add_argument("--vocab_size", type=int, required=True, help="Target vocabulary size")
     parser.add_argument("--save_name", type=str, required=True, help="Prefix for the saved vocab/merges files")
     # 可选参数
+    parser.add_argument("--bpe_version", type=str, default="fast",
+                        help="Select a version of bpe for training, using fast as default")
     parser.add_argument("--special_tokens", nargs="+", default=["<|endoftext|>"], help="List of special tokens")
 
     args = parser.parse_args()
@@ -23,24 +27,32 @@ def main():
     print(f"Input: {args.input_path}")
     print(f"Vocab Size: {args.vocab_size}")
     print(f"Special Tokens: {args.special_tokens}")
+    print(f"BPE version: {args.bpe_version}")
     print(f"Save Prefix: {args.save_name}")
     print(f"--------------------------------")
 
+    version: Literal["fast", "naive"] = args.bpe_version
     # 实例化并训练训练器
-    trainer = BPETokenizer.train(
-        input_path=args.input_path,
-        vocab_size=args.vocab_size,
-        special_tokens=args.special_tokens
-    )
+    if version == 'fast':
+        trainer = BPETokenizer.train(
+            input_path=args.input_path,
+            vocab_size=args.vocab_size,
+            special_tokens=args.special_tokens
+        )
+        vocab, merges = trainer._vocab, trainer._merges
+        print("实际训练出的vocab size: ", len(vocab))
+        print("实际训练出的merges size: ", len(merges))
 
-    # 获取vocab和merge，如果需要
-    # vocab, merge = trainer._vocab, trainer._merges
-
-    # 保存结果
-    if hasattr(trainer, 'save'):
         trainer.save(args.save_name)
+
     else:
-        print("警告: Trainer 没有实现 train 方法. 请先实现或者在调用train()时自动保存.")
+        vocab, merges = train_bpe_run(input_path=args.input_path,
+                                      vocab_size=args.vocab_size,
+                                      special_tokens=args.special_tokens)
+
+        print("实际训练出的vocab size: ", len(vocab))
+        print("实际训练出的merges size: ", len(merges))
+
 
 if __name__ == "__main__":
     main()
