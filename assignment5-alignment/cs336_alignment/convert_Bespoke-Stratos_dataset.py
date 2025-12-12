fixed bug in sft loss compute and attention mask

assignment5-alignment/cs336_alignment/convert_Bespoke-Stratos_dataset.py
@@ -5,17 +5,17 @@ import pandas as pd
 import numpy as np
 
 def prepare_bespoke_data_local():
-    local_data_dir = "data/Bespoke-Stratos/data"
-    print(f"🚀 正在读取本地 Parquet 文件: {local_data_dir} ...")
+    local_data_dir = "data/Bespoke-Stratos-17k/data"
+    print(f"正在读取本地 Parquet 文件: {local_data_dir} ...")
     
     parquet_files = glob.glob(os.path.join(local_data_dir, "train-*.parquet"))
     if not parquet_files:
-        print("❌ 错误: 未找到 Parquet 文件。")
+        print("错误: 未找到 Parquet 文件。")
         return
 
     df = pd.concat([pd.read_parquet(f) for f in parquet_files], ignore_index=True)
     
-    output_path = "data/MATH/sft.jsonl"
+    output_path = "data/MATH/sft-17k.jsonl"
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     
     count = 0
@@ -31,7 +31,6 @@ def prepare_bespoke_data_local():
             if hasattr(messages, '__len__') and len(messages) == 0:
                 continue
             
-            # 2. 【关键修复】处理 NumPy 数组和嵌套列表
             # 如果 messages 是 numpy array，先转为 list
             if isinstance(messages, np.ndarray):
                 messages = messages.tolist()
@@ -42,8 +41,8 @@ def prepare_bespoke_data_local():
             
             # --- 调试打印 (只打印第一条) ---
             if not debug_printed:
-                print(f"\n🔍 [DEBUG] 第一条数据的 messages 类型: {type(messages)}")
-                print(f"🔍 [DEBUG] 第一条数据的内容 (前100字符): {str(messages)[:100]}")
+                print(f"\n[DEBUG] 第一条数据的 messages 类型: {type(messages)}")
+                print(f"[DEBUG] 第一条数据的内容 (前100字符): {str(messages)[:100]}")
                 debug_printed = True
             # -----------------------------
 
@@ -72,7 +71,7 @@ def prepare_bespoke_data_local():
             # 4. 标签替换
             response = raw_response.replace("<|begin_of_thought|>\n\n", "<think>")
             response = response.replace("\n\n<|end_of_thought|>\n\n", "</think>")
-            response = response.replace("<|begin_of_solution|>\n\n", "<answer>")
+            response = response.replace("<|begin_of_solution|>\n\n", " <answer>")
             response = response.replace("\n\n<|end_of_solution|>", "</answer>")
             
             response = response.strip()
@@ -85,19 +84,19 @@ def prepare_bespoke_data_local():
             f.write(json.dumps(entry) + "\n")
             count += 1
             
-    print(f"\n✅ SFT 数据准备完毕！")
+    print(f"\nSFT 数据准备完毕！")
     print(f"共转换有效数据: {count} 条")
     print(f"文件已保存至: {output_path}")
     
     # 预览结果
     if count > 0:
-        print("\n🔍 最终数据预览:")
+        print("\n最终数据预览:")
         with open(output_path, "r") as f:
             first_line = json.loads(f.readline())
             print(first_line["prompt"])
             print(first_line["response"][:200] + "...")
     else:
-        print("⚠️ 警告：依然没有提取到数据，请检查上方的 [DEBUG] 信息。")
+        print("警告：依然没有提取到数据，请检查上方的 [DEBUG] 信息。")
 
 def check_sft_data(output_path : str):
      with open(output_path, "r") as f:
@@ -110,6 +109,6 @@ def check_sft_data(output_path : str):
         
 
 if __name__ == "__main__":
-    # prepare_bespoke_data_local()
-    output_path = "data/MATH/sft.jsonl"
-    check_sft_data(output_path)
+    prepare_bespoke_data_local()
+    # output_path = "data/MATH/sft.jsonl"
+    # check_sft_data(output_path)

assignment5-alignment/cs336_alignment/sft.py
@@ -28,6 +28,7 @@ def tokenize_prompt_and_output(prompt_strs: List[str], output_strs: List[str], t
         #     output_ids.append(eos_id)
         # else:
         #     pass
+        output_ids.append(tokenizer.eos_token_id)
         token_ids = prompt_ids + output_ids
         output_mask = [0] * len(prompt_ids) + [1] * len(output_ids)
         if len(token_ids) > max_length:
@@ -39,30 +40,38 @@ def tokenize_prompt_and_output(prompt_strs: List[str], output_strs: List[str], t
 
     max_len = max(len(ids) for ids in batch_tokens)
     padded_input_ids = []
-    padded_masks = []
-    padded_labels = []
+    padded_response_masks = []
+    padded_attention_masks = []
     for input_ids, masks in zip(batch_tokens, batch_mask):
         pad_len = max_len - len(input_ids)
         padded_input = F.pad(input=input_ids, pad=(0, pad_len), value=tokenizer.pad_token_id) # 对最后一个dim,左填充0个，右填充pad_len个
-        padded_mask = F.pad(input=masks, pad=(0, pad_len), value=0)
+        pad_res_mask = F.pad(input=masks, pad=(0, pad_len), value=0)
+
+        attn_mask = torch.cat([torch.ones(len(input_ids), dtype=torch.long), torch.zeros(pad_len, dtype=torch.long)])
 
         padded_input_ids.append(padded_input)
-        padded_masks.append(padded_mask)
+        padded_response_masks.append(pad_res_mask)
+        padded_attention_masks.append(attn_mask)
+
 
     batch_input_ids = torch.stack(padded_input_ids)
-    batch_masks = torch.stack(padded_masks)
+    batch_response_masks = torch.stack(padded_response_masks)
+    batch_attention_masks = torch.stack(padded_attention_masks)
 
     shifted_inputs = batch_input_ids[:, :-1]
-    shifted_masks = batch_masks[:, 1:]
+    shifted_attn_masks = batch_attention_masks[:, :-1]
+
     labels = batch_input_ids[:, 1:]
+    shifted_response_masks = batch_response_masks[:, :-1]
 
     return {
         "input_ids": shifted_inputs,
+        "attention_mask": shifted_attn_masks,
         "labels": labels,
-        "response_mask": shifted_masks
+        "response_mask": shifted_response_masks
     }
 
-def get_response_log_probs(model: torch.nn.Module, input_ids:torch.Tensor, labels: torch.Tensor, return_token_entropy: bool = False) -> Dict[str, torch.Tensor]:
+def get_response_log_probs(model: torch.nn.Module, input_ids:torch.Tensor, attention_masks: torch.Tensor, labels: torch.Tensor, return_token_entropy: bool = False) -> Dict[str, torch.Tensor]:
     """
     Args:
         model: PreTrainedModel, HuggingFace model used for scoring (placed on the correct device and in inference mode if gradients should not be computed).
@@ -76,11 +85,11 @@ def get_response_log_probs(model: torch.nn.Module, input_ids:torch.Tensor, label
     Returns:
         dict[str, torch.Tensor].
         "log_probs" shape (batch_size, sequence_length), conditional log-probabilities
-        log pθ(xt | x<t).
+        log pθ(xt | x<t).compute_generation_entropy
         "token_entropy" optional, shape (batch_size, sequence_length), per-token entropy
         for each position (present only if return_token_entropy=True).
     """
-    outputs = model.forward(input_ids)
+    outputs = model.forward(input_ids=input_ids, attention_masks=attention_masks)
     logits = outputs.logits # shape(batch_size, seq_len, vocab_size)
 
     all_log_probs = F.log_softmax(logits.to(torch.float32), dim=-1)
@@ -116,6 +125,29 @@ def masked_normalize(tensor: torch.Tensor, mask: torch.Tensor, normalize_constan
 
     return summation / normalize_constant
 
+def compute_generation_entropy(scores: tuple | None) -> float:
+    """
+    从 generate 返回的 scores 中计算平均熵。
+    Args:
+        scores: tuple of torch.FloatTensor (one for each step)
+    """
+    if not scores:
+        return 0.0ww
+    
+    # 1. 堆叠并转精度: (seq_len, batch_size, vocab_size) -> (seq_len, vocab_size)
+    stacked_logits = torch.stack(scores).squeeze(1).to(torch.float32)
+    
+    # 2. 计算概率
+    # probs = F.softmax(stacked_logits, dim=-1)
+    log_probs = F.log_softmax(stacked_logits, dim=-1)
+    probs = torch.exp(log_probs) 
+    # 3. 计算熵: -sum(p * log_p)
+    token_entropies = -torch.sum(probs * log_probs, dim=-1)
+
+    token_entropies = torch.nan_to_num(token_entropies, nan=0.0)
+    
+    return torch.nan_to_num(token_entropies, nan=0.0).mean().item()
+
 def sft_microbatch_train_step(policy_log_probs: torch.Tensor, response_mask: torch.Tensor, gradient_accumulation_steps: int, normalize_constant: int) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
     """
     Execute a forward-and-backward pass on a microbatch.
@@ -131,9 +163,12 @@ def sft_microbatch_train_step(policy_log_probs: torch.Tensor, response_mask: tor
     """
     batch_size = policy_log_probs.shape[0]
     pertoken_loss = -policy_log_probs
-    masked_loss = response_mask * pertoken_loss
-    sum_loss = masked_normalize(masked_loss, response_mask, normalize_constant)
-    mean_loss = sum_loss / batch_size
+    # masked_loss = response_mask * pertoken_loss
+    total_valid_tokens = response_mask.sum().item()
+    if total_valid_tokens == 0:
+        total_valid_tokens = 1
+
+    mean_loss = masked_normalize(pertoken_loss, response_mask, normalize_constant=total_valid_tokens,dim=None)
     actual_loss = mean_loss / gradient_accumulation_steps
     actual_loss.backward()
     log = {
@@ -149,7 +184,7 @@ def log_generations(
     ground_truths: List[str],
     reward_fn: Callable[[str, str], Dict[str, float]],
     num_examples_to_log: int = 4,
-    max_new_tokens: int = 1024,  # 抽查时可以设短一点节省时间，或者保持 1024
+    max_new_tokens: int = 1024,  
 ) -> Dict[str, float]:
     """
     使用 PyTorch 原生 generate 进行抽样评估，计算奖励和熵。
@@ -164,7 +199,6 @@ def log_generations(
     device = model.device
     was_training = model.training
     model.eval()  # 切换到评估模式
-
     total_rewards = []
     format_rewards = []
     answer_rewards = []
@@ -202,8 +236,7 @@ def log_generations(
         # 解析生成的文本 (去掉 Prompt 部分)
         generated_ids = outputs.sequences[0][input_len:]
         generated_text = tokenizer.decode(generated_ids, skip_special_tokens=True)
-        
-        # --- 计算指标 ---
+
         
         # A. 长度
         lengths.append(len(generated_ids))
@@ -211,23 +244,33 @@ def log_generations(
         # B. 熵 (Entropy)
         # outputs.scores 是一个 tuple，长度为 generated_len
         # 每个元素是 (batch_size=1, vocab_size) 的 logits
-        if outputs.scores:
-            # 堆叠为 (Seq, Vocab)
-            stacked_logits = torch.stack(outputs.scores).squeeze(1)
-            # 计算 Prob 和 LogProb
-            probs = torch.nn.functional.softmax(stacked_logits, dim=-1)
-            log_probs = torch.nn.functional.log_softmax(stacked_logits, dim=-1)
-            # 熵公式: -sum(p * log_p)
-            token_entropies = -(probs * log_probs).sum(dim=-1)
-            # 取平均
-            avg_entropy = token_entropies.mean().item()
-            entropies.append(avg_entropy)
-        else:
-            entropies.append(0.0)
+        # if outputs.scores:
+        #     # 堆叠为 (Seq, Vocab)
+        #     stacked_logits = torch.stack(outputs.scores).squeeze(1).to(torch.float32)
+        #     # 计算 Prob 和 LogProb
+        #     probs = torch.nn.functional.softmax(stacked_logits, dim=-1)
 
+        #     top_prob, _ = torch.max(probs, dim=-1)
+        #     if i == 0: # 只打印第一条样本的
+        #         print(f"DEBUG: Max prob of first token: {top_prob[0].item():.6f}")
+        #         if top_prob[0].item() > 0.9999:
+        #             print("DEBUG: Model is extremely confident (Determininstic)!")
+
+        #     log_probs = torch.nn.functional.log_softmax(stacked_logits , dim=-1)
+        #     # 熵公式: -sum(p * log_p)
+        #     token_entropies = -(probs * log_probs).sum(dim=-1)
+        #     token_entropies = torch.nan_to_num(token_entropies, nan=0.0)
+        #     # 取平均
+        #     avg_entropy = token_entropies.mean().item()
+        #     entropies.append(avg_entropy)
+        # else:
+        #     entropies.append(0.0)
+
+        entropy = compute_generation_entropy(outputs.scores)
+        entropies.append(entropy)
         # C. 奖励 (Reward)
         # 假设 reward_fn 接受 (output, truth)
-        metrics = reward_fn(generated_text, truth)
+        metrics = reward_fn(generated_text.replace("</think><answer>", "</think> <answer>"), truth)
         total_rewards.append(metrics.get("reward", 0.0))
         format_rewards.append(metrics.get("format_reward", 0.0))
         answer_rewards.append(metrics.get("answer_reward", 0.0))

assignment5-alignment/cs336_alignment/train_grpo.py
@@ -1,6 +1,5 @@
 import os
 os.environ["VLLM_ALLOW_INSECURE_SERIALIZATION"] = "1"
-# 2. 解决 CUDA 碎片化 (针对本次报错)
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 import re
 import yaml
@@ -12,7 +11,8 @@ import numpy as np
 from tqdm import tqdm
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers.models.auto.tokenization_auto import AutoTokenizer
+from transformers.models.auto.modeling_auto import AutoModelForCausalLM
 from vllm import LLM, SamplingParams
 # 引入组件
 from cs336_alignment.sft import (
@@ -262,6 +262,7 @@ def train(config_path: str):
     # 开启梯度检查点省显存
     policy.gradient_checkpointing_enable()
     policy.train()
+
     
     optimizer = AdamW(policy.parameters(), lr=float(config["training"]["learning_rate"]))
 
@@ -286,7 +287,8 @@ def train(config_path: str):
         max_tokens=config["training"]["sampling_max_tokens"],
         stop=["</answer>"],
         include_stop_str_in_output=True,
-        n=config["training"]["group_size"] # 一次生成 G 个
+        n=config["training"]["group_size"], # 一次生成 G 个
+        repetition_penalty=config["training"]["repetition_penalty"],
     )
 
     # --- Data ---
@@ -303,10 +305,10 @@ def train(config_path: str):
     
     # --- GRPO Loop ---
     n_grpo_steps = config["training"]["n_grpo_steps"]
-    grad_accum_steps = config["training"]["gradient_accumulation_steps"]
     micro_batch_size = config["training"]["micro_batch_size"]
     epochs_per_batch = config["training"]["epochs_per_rollout_batch"]
     clip_range = config["training"]["clip_range"]
+    normalize_by_std = config["training"].get("normalize_by_std", True)
     
     start_step = config["model"]["start_step"]
     global_step = start_step if start_step is not None else 0
@@ -368,7 +370,7 @@ def train(config_path: str):
             repeated_ground_truths=all_ground_truths,
             group_size=config["training"]["group_size"],
             advantage_eps=config["training"]["advantage_eps"],
-            normalize_by_std=True
+            normalize_by_std=normalize_by_std
         )
         # 转为 Tensor 并移到 GPU
         advantages = advantages.to(device).unsqueeze(1) # (B*G, 1)
@@ -398,6 +400,7 @@ def train(config_path: str):
         input_ids = tokenized_batch["input_ids"].to(device)
         response_mask = tokenized_batch["response_mask"].to(device)
         labels = tokenized_batch["labels"].to(device)
+        attention_mask = tokenized_batch["attention_mask"].to(device)
         
         # 1.6 (Optional) Get Old Log Probs
         # 对于 Reinforce 其实不需要，但为了兼容 Clip Loss，这里通常会计算一次
@@ -410,8 +413,9 @@ def train(config_path: str):
             for i in range(0, len(input_ids), inference_batch_size):
                 batch_input_ids = input_ids[i : i + inference_batch_size]
                 batch_labels = labels[i : i + inference_batch_size]
+                batch_mask = attention_mask[i: i + inference_batch_size]
                 
-                log_probs_dict = get_response_log_probs(policy, batch_input_ids, batch_labels)
+                log_probs_dict = get_response_log_probs(policy, batch_input_ids, batch_mask, batch_labels)
                 old_log_probs_list.append(log_probs_dict["log_probs"].detach())
                 
         old_log_probs = torch.cat(old_log_probs_list, dim=0)
@@ -426,12 +430,19 @@ def train(config_path: str):
         indices = torch.randperm(train_dataset_len)
         
         policy.train()
-        
         # 记录累积 loss
         step_loss = 0.0
-        accum_stats = {"loss": 0.0, "clip_ratio": 0.0, "approx_kl": 0.0}
+        optimizer.zero_grad()
+        # accum_stats = {"loss": 0.0, "clip_ratio": 0.0, "approx_kl": 0.0}
         # Inner Epochs (On-policy 通常是 1)
-        for _ in range(epochs_per_batch):
+        for epoch_idx in range(epochs_per_batch):
+            indices = torch.randperm(train_dataset_len)
+            actual_accum_steps = train_dataset_len // micro_batch_size
+            epoch_metrics = {"loss": [], "clip_ratio": [], "approx_kl": []}
+            current_epoch_loss = 0.0
+            current_epoch_clip = 0.0
+            current_epoch_kl = 0.0
+
             for i in range(0, train_dataset_len, micro_batch_size):
                 mb_idx = indices[i : i + micro_batch_size]
                 
@@ -441,17 +452,18 @@ def train(config_path: str):
                 mb_mask = response_mask[mb_idx]
                 mb_adv = advantages[mb_idx]
                 mb_old_lp = old_log_probs[mb_idx]
+                mb_attention_mask = attention_mask[mb_idx]
                 # mb_rewards = raw_rewards[mb_idx] # 如果是 no_baseline 需要这个
                 
                 # Forward
-                mb_log_probs_dict = get_response_log_probs(policy, mb_input_ids, mb_labels)
+                mb_log_probs_dict = get_response_log_probs(policy, mb_input_ids, mb_attention_mask, mb_labels)
                 mb_policy_log_probs = mb_log_probs_dict["log_probs"]
                 
                 # GRPO Backward
                 # 注意：grad_accum_steps 这里需要怎么算？
                 # 我们希望每一轮 Rollout 更新一次参数。
                 # 所以 accum_steps 应该是 (rollout_batch_size / micro_batch_size)
-                actual_accum_steps = train_dataset_len // micro_batch_size
+                
                 
                 loss, step_metrics = grpo_microbatch_train_step(
                     policy_log_probs=mb_policy_log_probs,
@@ -463,9 +475,9 @@ def train(config_path: str):
                     cliprange=clip_range
                     # raw_rewards=mb_rewards
                 )
-                accum_stats["loss"] += loss.item() / actual_accum_steps
-                accum_stats["clip_ratio"] += step_metrics["clip_ratio"].item() / actual_accum_steps
-                accum_stats["approx_kl"] += step_metrics["approx_kl"].item() / actual_accum_steps
+                current_epoch_loss += loss.item()
+                current_epoch_clip += step_metrics["clip_ratio"].item() / actual_accum_steps
+                current_epoch_kl += step_metrics["approx_kl"].item() / actual_accum_steps
                 
                 step_loss += loss.item() / actual_accum_steps
 
@@ -473,7 +485,9 @@ def train(config_path: str):
             grad_norm = torch.nn.utils.clip_grad_norm_(policy.parameters(), config["training"]["max_grad_norm"])
             optimizer.step()
             optimizer.zero_grad()
-        
+            epoch_metrics["loss"].append(current_epoch_loss)
+            epoch_metrics["clip_ratio"].append(current_epoch_clip)
+            epoch_metrics["approx_kl"].append(current_epoch_kl)
         global_step += 1
         pbar.update(1)
         
@@ -486,9 +500,9 @@ def train(config_path: str):
             "train/format_rate": np.mean(format_scores),  # 格式正确率
             
             # 2. 训练动态
-            "train/loss": accum_stats["loss"],
-            "train/clip_fraction": accum_stats["clip_ratio"],
-            "train/approx_kl": accum_stats["approx_kl"],
+            "train/loss": np.mean(epoch_metrics["loss"]),
+            "train/clip_fraction": np.mean(epoch_metrics["clip_ratio"]),
+            "train/approx_kl": np.mean(epoch_metrics["approx_kl"]),
             "train/lr": optimizer.param_groups[0]['lr'],
             "train/grad_norm": grad_norm.item(), # 之前代码里算的
             
@@ -503,7 +517,6 @@ def train(config_path: str):
 
         # --- Evaluation & Saving ---
         if global_step % config["evaluation"]["eval_every_steps"] == 0:
-            # ... 调用 log_generations (和 SFT 一样) ...
             policy.eval()
             
             eval_max_tokens = config["evaluation"].get("max_new_tokens", 2048)
@@ -536,8 +549,6 @@ def train(config_path: str):
             # 在 WandB 里打个标记
             wandb.log({"train/best_reward": best_reward, "train/global_step": global_step}, commit=False)
 
-        # B. 定期保存 (Periodic Save) - 防止意外中断
-        # 建议在 config 里把 save_steps 设小一点，比如 50 或 100
         if global_step % config["training"]["save_steps"] == 0:
             print(f"定期保存中 {global_step}")
             

assignment5-alignment/cs336_alignment/train_sft.py
@@ -10,6 +10,7 @@ from transformers.models.auto.tokenization_auto import AutoTokenizer
 from transformers.models.auto.modeling_auto import AutoModelForCausalLM
 from torch.optim import AdamW
 
+
 from cs336_alignment.drgrpo_grader import r1_zero_reward_fn
 from cs336_alignment.sft import get_response_log_probs, tokenize_prompt_and_output, sft_microbatch_train_step, log_generations
 
@@ -62,7 +63,7 @@ def get_collate_fn(tokenizer, max_length = 1024, prompt_template = None):
         
     return collate_fn
 
-def train(config_path: str):
+def train(config_path: str, args):
     # --- 加载配置 ---
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
@@ -71,17 +72,26 @@ def train(config_path: str):
     print(json.dumps(config, indent=2))
 
     # --- WandB 初始化 ---
-    wandb.init(
-        project=config["wandb"]["project"],
-        name=config["wandb"]["run_name"],
-        config=config
-    )
+    if args.wandb_id:
+        print(f"Resuming WandB run: {args.wandb_id}")
+        wandb.init(
+            project=config["wandb"]["project"],
+            id=args.wandb_id,   # 指定 ID
+            resume="must",      # 强制续训，如果ID不存在会报错
+            config=config
+        )
+    else:
+        wandb.init(
+            project=config["wandb"]["project"],
+            name=config["wandb"]["run_name"],
+            config=config
+        )
 
     # --- 路径与设备 ---
     output_dir = config["training"]["output_dir"]
     os.makedirs(output_dir, exist_ok=True)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
+    model_load_path = args.resume_from if args.resume_from else config["model"]["model_path"]
     # --- 加载模型与 Tokenizer ---
     print("Loading model and tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(config["model"]["model_path"])
@@ -89,12 +99,13 @@ def train(config_path: str):
         tokenizer.pad_token = tokenizer.eos_token
         
     model = AutoModelForCausalLM.from_pretrained(
-        config["model"]["model_path"],
+        model_load_path,
         torch_dtype=getattr(torch, config["model"]["dtype"]), # 动态获取 torch.bfloat16
         attn_implementation=config["model"]["attn_implementation"],
         device_map="auto"
     )
     model.gradient_checkpointing_enable()
+    model.config.use_cache = False
     model.train()
 
     # --- 准备数据 ---
@@ -132,41 +143,53 @@ def train(config_path: str):
     grad_accum_steps = config["training"]["gradient_accumulation_steps"]
     clip_norm = config["training"]["max_grad_norm"]
     eval_every = config["evaluation"]["eval_every_steps"]
+    save_interval = config["training"]["save_steps"]
     
     global_step = 0
     total_micro_steps = 0
+    accumulated_loss = 0.0
     
+    total_global_steps = len(train_loader) * epochs // grad_accum_steps
     print("开始训练")
-    
-    for epoch in range(epochs):
+    start_epoch = args.start_epoch
+    steps_per_epoch = len(train_loader) // grad_accum_steps
+    global_step = start_epoch * steps_per_epoch 
+    for epoch in range(start_epoch, epochs):
         # 使用 tqdm 包装 loader 显示进度
-        progress_bar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}")
+        progress_bar = tqdm(
+            train_loader, 
+            desc=f"Epoch {epoch+1}/{epochs}",
+            ncols=120, # 限制宽度，防止在某些终端换行
+            leave=True 
+        )
+
+        epoch_loss_tracker = 0.0
         
         for batch in progress_bar:
             # 1. 搬运数据
             input_ids = batch["input_ids"].to(device)
             labels = batch["labels"].to(device)
             response_mask = batch["response_mask"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
 
             # 2. 获取 Log Probs
             # 注意：return_token_entropy=False 节省计算，SFT loss 不用它
-            log_probs_dict = get_response_log_probs(model, input_ids, labels)
+            log_probs_dict = get_response_log_probs(model, input_ids, attention_mask, labels)
             policy_log_probs = log_probs_dict["log_probs"]
             
             # 3. 计算 Loss 并 Backward
-            # sft_microbatch_train_step 内部会处理 loss / accum_steps
-            # 并执行 .backward()
-            loss, metrics = sft_microbatch_train_step(
+            _, metrics = sft_microbatch_train_step(
                 policy_log_probs=policy_log_probs,
                 response_mask=response_mask,
                 gradient_accumulation_steps=grad_accum_steps,
                 normalize_constant=1
             )
             
+            accumulated_loss += metrics["loss"].item()
             total_micro_steps += 1
             
             # 更新进度条上的 Loss 显示
-            progress_bar.set_postfix(loss=metrics["loss"].item())
+            # progress_bar.set_postfix(loss=metrics["loss"].item())
 
             # 4. 梯度累积更新
             if total_micro_steps % grad_accum_steps == 0:
@@ -178,19 +201,32 @@ def train(config_path: str):
                 optimizer.zero_grad()
                 
                 global_step += 1
-                
+
+                avg_loss = accumulated_loss / grad_accum_steps
+                epoch_loss_tracker += avg_loss # 记录一下
                 # 记录日志 (记录真实的 batch loss)
+                progress_bar.set_postfix({
+                                    "Step": f"{global_step}/{total_global_steps}", # 显示全局进度
+                                    "Loss": f"{avg_loss:.4f}",    # 显示真实的 step loss
+                                    "Norm": f"{grad_norm.item():.2f}", # 监控梯度爆炸
+                                    "LR": f"{optimizer.param_groups[0]['lr']:.2e}" # 监控 LR 变化
+                                })
+                
                 wandb.log({
-                    "train/loss": metrics["loss"].item(),
+                    "train/loss": avg_loss,
                     "train/grad_norm": grad_norm.item(),
                     "train/global_step": global_step,
-                    "train/epoch": epoch + (total_micro_steps / len(train_loader))
+                    "train/epoch":  epoch + (progress_bar.n / len(train_loader)),
+                    "train/lr": optimizer.param_groups[0]['lr']
                 })
+
+                accumulated_loss = 0.0
 
                 # 5. 评估 (抽查生成)
                 if global_step % eval_every == 0:
-                    # torch.cuda.empty_cache()
-                    eval_max_tokens = config["evaluation"].get("max_new_tokens", 1024)
+                    torch.cuda.empty_cache()
+                    progress_bar.write(f"Step {global_step}: Running Evaluation...")
+                    eval_max_tokens = config["evaluation"].get("max_new_tokens", 2048)
                     eval_stats = log_generations(
                         model=model,
                         tokenizer=tokenizer,
@@ -204,18 +240,24 @@ def train(config_path: str):
                     # 合并日志
                     eval_stats["train/global_step"] = global_step
                     wandb.log(eval_stats)
-                    
+                    del eval_stats
+                    torch.cuda.empty_cache() 
                     model.train() # 切回训练模式
 
-    # --- 保存最终模型 ---
-    print(f"正在保存模型到{output_dir}...")
-    model.save_pretrained(output_dir)
-    tokenizer.save_pretrained(output_dir)
+        # --- 保存最终模型 ---
+        epoch_output_dir = os.path.join(output_dir, f'epoch{epoch}')
+        print(f"正在保存模型到{epoch_output_dir}...")
+        model.save_pretrained(epoch_output_dir)
+        tokenizer.save_pretrained(epoch_output_dir)
     print("训练完成")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="configs/sft_config.yaml", help="Path to YAML config file")
+
+    parser.add_argument("--resume_from", type=str, default=None, help="Path to the checkpoint directory (e.g., checkpoints/sft_v1/epoch0)")
+    parser.add_argument("--wandb_id", type=str, default=None, help="The ID of the wandb run to resume (e.g., a1b2c3d4)")
+    parser.add_argument("--start_epoch", type=int, default=0, help="The epoch number to start form (e.g., 1)")
     args = parser.parse_args()
     
-    train(args.config)
+    train(args.config, args)

assignment5-alignment/cs336_alignment/utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 def pertoken_entropy(logits: torch.Tensor) -> torch.Tensor:
-    log_prob = F.log_softmax(logits, dim = -1) # shape: batch_size, seq_len, vocab_size
-    explog_prob = torch.exp(log_prob)
-    multiplication = log_prob * explog_prob
-    return -torch.sum(multiplication, dim=-1) # batch_size, seq_len
+    log_probs = F.log_softmax(logits, dim = -1) # shape: batch_size, seq_len, vocab_size
+    probs = torch.exp(log_probs)
+    entropy = -torch.sum(probs * log_probs, dim=-1)
+    return torch.nan_to_num(entropy, nan=0.0)# batch_size, seq_len