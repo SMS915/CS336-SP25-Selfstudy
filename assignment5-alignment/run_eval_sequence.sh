767f676ecfae6ce591a07b25a8a37d32b2ba1540
justin <4409151130liu@gmail.com>
2025-12-28 14:53:52 +0800

    update structure of configs folder, add config for instruct model

--- assignment5-alignment/run_eval_sequence.sh
+++ assignment5-alignment/run_eval_sequence.sh
@@ -3,13 +3,12 @@
 echo "========== 开始批量评估任务: $(date) =========="
 
 echo ">>> [1/3] Running Baseline evaluation..."
-python cs336_alignment/evaluate_passk.py --config configs/evaluate_baseline_amc_pass64.yaml
-
+python cs336_alignment/evaluate_passk.py --config configs/eval/evaluate_baseline_amc_pass64.yaml
 
 echo ">>> [2/3] Running SFT evaluation..."
-python cs336_alignment/evaluate_passk.py --config configs/evaluate_sft_amc_pass64.yaml
+python cs336_alignment/evaluate_passk.py --config configs/eval/evaluate_sft_amc_pass64.yaml
 
 echo ">>> [3/3] Running Dr.GRPO evaluation..."
-python cs336_alignment/evaluate_passk.py --config configs/evaluate_drgrpo_amc_pass64.yaml
+python cs336_alignment/evaluate_passk.py --config configs/eval/evaluate_drgrpo_amc_pass64.yaml
 
 echo "========== 所有任务已完成: $(date) =========="
