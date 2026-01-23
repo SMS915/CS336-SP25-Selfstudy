9dce612ac4a76bf4fa93193e5bffb63bc5819f0f
Justin Liu <4409151130liu@gmail.com>
2026-01-24 00:58:28 +0800

    implemented mix precision benchmark

--- assignment2-systems/cs336_systems/benchmarking.py
+++ assignment2-systems/cs336_systems/benchmarking.py
@@ -1,3 +1,4 @@
+
 import yaml
 import timeit
 import torch
@@ -10,6 +11,8 @@ import cs336_basics.model
 from cs336_basics.utils import Softmax
 from einops import einsum, rearrange
 from jaxtyping import Float, Bool
+from typing import Optional
+from torch.optim import AdamW
 from math import sqrt
 
 from cs336_basics.model import ScaledDotProductAttention as original_SDPA
@@ -18,7 +21,7 @@ def annotated_scaled_dot_product_attention(
     Q: Float[torch.Tensor, "batch_size num_q q_seq_len d_q"],
     K: Float[torch.Tensor, "batch_size num_k k_seq_len d_k"],
     V: Float[torch.Tensor, "batch_size num_v v_seq_len d_v"],
-    mask: Bool[torch.Tensor, "batch_size q_seq_len k_seq_len"] = None
+    mask: Optional[Bool[torch.Tensor, "batch_size q_seq_len k_seq_len"]] = None
 )-> Float[torch.Tensor, "batch_size q_seq_len d_v"]:
     with nvtx.range("ScaledDotProductAttention"):
         is_gqa = False
@@ -67,6 +70,9 @@ def main():
     parser.add_argument("--warmup_steps", type=int, default=5)
     parser.add_argument("--measure_steps", type=int, default=10)
 
+    parser.add_argument("--optimize", action='store_true')
+    parser.add_argument('--precision', type=str, default='autocast', choices=['fp32', 'fp16', 'bf16', 'autocast'],
+                    help='Training precision. "autocast" will automatically choose the best available.')
     args = parser.parse_args()
 
     with open(args.config_path, "r") as f:
@@ -104,14 +110,54 @@ def main():
     
     model.to('cuda')
     # model.count_params()
+    optimize = args.optimize == True
+    if optimize:
+        optimizer = AdamW(model.parameters(),
+                          lr = 0.001,
+                          weight_decay=0.1,
+                          betas=(0.95, 0.99)
+                          )
+        
+
+    amp_enabled = True
+    if args.precision == 'fp32':
+        amp_enabled = False
+        amp_dtype = torch.float32
+        use_scaler = False
+    elif args.precision == 'fp16':
+        amp_dtype = torch.float16
+        use_scaler = True
+    elif args.precision == 'bf16':
+        if torch.cuda.is_bf16_supported():
+            amp_dtype = torch.bfloat16
+            use_scaler = False
+        else:
+            print("Warning: BF16 not supported on this device, falling back to FP16.")
+            amp_dtype = torch.float16
+            use_scaler = True
+    elif args.precision == 'autocast':
+        amp_dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
+        use_scaler = (amp_dtype == torch.float16)
+
+    if amp_enabled:
+        print(f"AMP enabled, dtype: {amp_dtype}, using GradScaler: {use_scaler}")
+    else:
+        print("AMP disabled, running in FP32.")
+
+    scaler = torch.amp.grad_scaler.GradScaler(enabled=use_scaler)
+
     batched_data = torch.randint(0, vocab_size, (batch_size, context_length)).to('cuda')
 
     print("Warmup Stage")
     warmup_start = timeit.default_timer()
     for _ in range(warmup_steps):
+        optimizer.zero_grad()
         output = model(batched_data)
         loss = output.sum()
         loss.backward()
+        scaler.unscale_(optimizer)
+        scaler.step(optimizer)
+        scaler.update()
 
     # 同步，确保预热完成
     torch.cuda.synchronize()
@@ -123,26 +169,42 @@ def main():
     measure_steps = args.measure_steps
     forward_timings = []
     backward_timings = []
+    optimize_timings = []
     total_timings = []
+    
     print(f"Measuring for {measure_steps} steps")
     for _ in range(measure_steps):
+        if optimize:
+            optimizer.zero_grad()
         start_time = timeit.default_timer()
-
-        # 运行测量操作
-        output = model(batched_data)
+        with nvtx.range("Forward Pass"):
+            with torch.amp.autocast_mode.autocast(device_type='cuda', enabled=amp_enabled, dtype=amp_dtype):
+                output = model(batched_data)        
         torch.cuda.synchronize()
 
         forward_end = timeit.default_timer()
-
-        loss = output.sum()
-        loss.backward()
+        with nvtx.range("Backward Pass"):
+            loss = output.sum()
+            scaler.scale(loss).backward()
         torch.cuda.synchronize()
-
+        backward_end = timeit.default_timer()
+        
+        if optimize:
+            with nvtx.range("Optimizer step"):
+                scaler.unscale_(optimizer)
+                scaler.step(optimizer)
+                scaler.update()
+                torch.cuda.synchronize()
+                optimize_end = timeit.default_timer()
+        
         end_time = timeit.default_timer()
 
         forward_timings.append(forward_end - start_time)
-        backward_timings.append(end_time - forward_end)
+        backward_timings.append(backward_end - forward_end)
         total_timings.append(end_time - start_time)
+
+        if optimize:
+            optimize_timings.append(optimize_end - backward_end)
 
     forward_mean = np.mean(forward_timings)
     forward_std = np.std(forward_timings)
@@ -153,11 +215,16 @@ def main():
     total_mean = forward_mean + backward_mean
     total_std = np.std(total_timings)
 
+    if optimize:
+        optimize_mean = np.mean(optimize_timings)
+        optimize_std = np.mean(optimize_timings)
     print("Measurement ends!")
     print(f"Takes {np.sum(total_timings):.2f} secs in total")
     print(f"For whole progress, the average time is {total_mean:.2f}, standard deviation is {total_std:.2f}.")
-    print(f"For forward pass ,the average time is {forward_mean:.2f}, standard deviation is {forward_std:.2f}.")
-    print(f"For forward pass ,the average time is {backward_mean:.2f}, standard deviation is {backward_std:.2f}.")
+    print(f"For forward pass, the average time is {forward_mean:.2f}, standard deviation is {forward_std:.2f}.")
+    print(f"For backward pass, the average time is {backward_mean:.2f}, standard deviation is {backward_std:.2f}.")
+    if optimize:
+        print(f"For optimizer step ,the average time is {optimize_mean:.2f}, standard deviation is {optimize_std:.2f}.")
 
 
 if __name__ == '__main__':
